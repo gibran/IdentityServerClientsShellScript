c6d7715cc22a22785faeecdcb3ee9b256f80d755
Gibran Silva <gibran.silva@gmail.com>
2015-11-24 00:44:59 -0200

    Use Cookie Token
    Cookie to control when will be need renew or obtain new access token

--- ClientCredential.sh
+++ ClientCredential.sh
@@ -1,3 +1,6 @@
+myCookie=~/.`basename $0`
+cookie_is_valid=0
+
 client_id='<CLIENT_ID>'
 client_secret='<SECRETE>'
 scope='<SCOPE>'
@@ -10,16 +13,37 @@ dataJSON="{ }"
 echo "Authenticating in:"
 echo "$urlToken?client_id=$client_id&scope=$scope"
 
-auth_result=$(curl -k -s $urlToken \
-	-H "Content-Type: application/x-www-form-urlencoded" \
-	-d scope=$scope \
-	-d client_id=$client_id \
-	-d client_secret=$client_secret \
-	-d grant_type=client_credentials)
+if [ -f $myCookie ]; then #Validate Cookie
+	. $myCookie
+	time_now=`date +%s`
+	
+	if (( time_now < expires_at )); then cookie_is_valid=1; fi
+fi
 
-access_token=$(echo -e "$auth_result" | \
-			grep -Po '"access_token" *: *.*?[^\\]",' | \
-			awk -F'"' '{ print $4 }')
+if !(( cookie_is_valid )); then
+	#Renew Access Token
+	
+	auth_result=$(curl -k -s $urlToken \
+		-H "Content-Type: application/x-www-form-urlencoded" \
+		-d scope=$scope \
+		-d client_id=$client_id \
+		-d client_secret=$client_secret \
+		-d grant_type=client_credentials)
+
+	access_token=$(echo -e "$auth_result" | \
+				grep -Po '"access_token" *: *.*?[^\\]",' | \
+				awk -F'"' '{ print $4 }')
+
+	expires_in=$(echo -e "$auth_result" | \
+				grep -Po '"expires_in"*: *.*,' | \
+				awk -F'"' '{ print $3 }' | \
+				awk -F':' '{ print $2}' | \
+				awk -F',' '{ print $1}' )
+	time_now=`date +%s`
+
+	expires_at=$((time_now + expires_in - 60))
+	echo -e "access_token=$access_token\nexpires_at=$expires_at" > $myCookie
+fi
 
 if [ -n "$access_token" ]; then	
 	data_result=$(curl -X POST -s $urlApi \
